ea857b679e128456a5c5586b64cdb08315ebd5c6
KatyaNiosi <plimmi.1@gmail.com>
2018-09-26 19:31:45 -0700

    Yata

--- phase2/syscalls.c
+++ phase2/syscalls.c
@@ -28,10 +28,23 @@ int GetPid(void) {
 }
 
 void SetVideo(int row, int col) {
-
+    asm("movl %0, %%eax;
+         movl %1, %%ebx;
+         movl %2, %%ecx;
+         int $128"
+        : 
+        : "g" (SETVIDEO), "g" (row), "g" (col)
+        : "eax", "ebx", "ecx")
+         
 }
 
 void Write(int device, char *str) {
+    asm("movl %0, %%eax;
+         movl %1, %%ebx;
+         movl %2, %%ecx;
+         int $128"
+         :
+         : "g" (WRITE), "g"(device), "g"(str)
+         :"eax", "ebx", "ecx")
 
 }
-
