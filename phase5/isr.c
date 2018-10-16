8ba187ef29306245e64dccd7c3fa330104074639
KatyaNiosi <plimmi.1@gmail.com>
2018-10-15 18:47:35 -0700

    term

--- phase5/isr.c
+++ phase5/isr.c
@@ -232,8 +232,14 @@ void TermRxISR(int index){
   int pid;
   char inport;
   inport = (char)inportb(term_if[index].io + IIR);
+  if(inport == '\n' || inport '\r'){
+    outportb(term_if[index].io, inport);
+    if(term_if[index].rx_wait_q.size != 0){
+      term_if[index].rx_p++;
+    }
+    return;
+  }
   
-
 
 }
 
